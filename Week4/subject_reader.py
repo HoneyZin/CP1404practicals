050c2758cfe8a5435a487af5733c1e329bb63ae7
Honey Zin <honey27zin@gmail.com>
2022-04-07 09:48:19 +0800

    Refactor and add new function

--- Week4/subject_reader.py
+++ Week4/subject_reader.py
@@ -7,13 +7,16 @@ FILENAME = "subject_data.txt"
 
 
 def main():
+    """Convert data strings into lists and display."""
     data = get_data()
     print(data)
+    subject_info(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    list_of_lists = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,7 +26,15 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        list_of_lists.append(parts)
     input_file.close()
+    return list_of_lists
+
+
+def subject_info(lists):
+    """Display subject details."""
+    for element in lists:
+        print("{} is taught by {:12} and has {:3} students".format(element[0], element[1], element[2]))
 
 
 main()
