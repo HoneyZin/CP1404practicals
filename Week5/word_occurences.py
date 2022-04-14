f977e141c9a82abd9ed5ae2fab74a275a1e8a412
Honey Zin <honey27zin@gmail.com>
2022-04-14 10:39:36 +0800

    program to count word occurrences

---
+++ Week5/word_occurences.py
@@ -0,0 +1,21 @@
+"""Program to count the occurrences of words in a string"""
+
+
+def main():
+    """Get user input and count the occurrences of words."""
+    sentence = input("Text: ")
+    words = sentence.split()
+    occurrences = {}
+    for word in words:
+        occurrences[word] = occurrences.get(word, 0) + 1
+
+    words = list(occurrences.keys())  # Sorting the keys
+    words.sort()
+    length = max(len(word) for word in words)
+    # Print the sorted keys and the values
+    for word in words:
+        print(f"{word:<{length}} : {occurrences[word]}")
+
+
+if __name__ == '__main__':
+    main()
