45ce9d3380284e58d87acdbffd0b203efeaccfd4
Honey Zin <honey27zin@gmail.com>
2022-05-19 09:25:31 +0800

    Add sorting files 2

---
+++ Week9/sort_files_2.py
@@ -0,0 +1,26 @@
+import os
+
+
+def main():
+    os.chdir("FilesToSort")
+    print(os.getcwd())
+    categories = []
+
+    for filename in os.listdir('.'):
+        if os.path.isdir(filename):
+            continue
+        extension = filename.split('.')
+        file_extension = extension[1]
+        if file_extension not in categories:
+            category = input(f"What category would you like to sort {file_extension} files into?")
+            categories.append(category)
+            try:
+                os.mkdir(file_extension)
+            except FileExistsError:
+                pass
+        print(f"{file_extension}/{filename}")
+        os.rename(filename, f"{file_extension}/{filename}")
+
+
+if __name__ == '__main__':
+    main()
